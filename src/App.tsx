33c8f60ef56891be572df146393be925bc42e5ec
Patel-pal <patelpal.93130@gmail.com>
2025-06-25 09:53:06 +0530

    first commit 25/06/25

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ function App() {
 
             <div className="relative w-full h-0.5">
               <button
-                type="button"
+                type="button" 
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
                 onClick={swap}
               >
@@ -73,7 +73,7 @@ function App() {
 
             <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
               Convert {from.toUpperCase()} to {to.toUpperCase()}
-            </button>
+            </button> 
           </form>
         </div>
       </div>
